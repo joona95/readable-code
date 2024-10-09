2735961c5e8b43a1b4c0c45bdb624d4eb35d9590
Joona Choi <juna052@naver.com>
2024-10-09 18:15:51 +0900

    fix: 깃발 다 꽂으면 이기는 오류 수정
    - 지뢰 셀 여부에 따라 체크 여부 조건 수정

--- src/main/java/cleancode/minesweeper/tobe/minesweeper/board/cell/CellState.java
+++ src/main/java/cleancode/minesweeper/tobe/minesweeper/board/cell/CellState.java
@@ -22,10 +22,6 @@ public class CellState {
         return isOpened;
     }
 
-    public boolean isChecked() {
-        return isFlagged || isOpened;
-    }
-
     public void flag() {
         this.isFlagged = true;
     }
